dcec57b861198381c6723200c86ee6147e34fca9
Daniela Sanchez Ocana <43196655+Daniela-Sanchez@users.noreply.github.com>
2019-06-03 11:38:05 -0500

    haha

---
+++ cssenv/cssenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\valedo\documents\github\clienteservidor\cssenv\cssenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
